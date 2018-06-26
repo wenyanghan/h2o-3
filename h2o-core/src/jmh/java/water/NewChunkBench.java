e1216aee98411f83ba098df80b58bdfb6f183341
wendycwong <wendycwong@yahoo.com>
2018-06-25 17:13:23 -0700

    Change it back.

--- h2o-core/src/jmh/java/water/NewChunkBench.java
+++ h2o-core/src/jmh/java/water/NewChunkBench.java
@@ -8,13 +8,9 @@ import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 import water.fvec.Chunk;
-import water.fvec.Frame;
-import water.fvec.Vec;
+import water.fvec.NewChunk;
 
 import java.util.concurrent.TimeUnit;
-
-import static org.junit.Assert.assertTrue;
-import static water.TestUtil.parse_test_file;
 
 /**
  * Chunk access patterns benchmark
@@ -26,190 +22,138 @@ import static water.TestUtil.parse_test_file;
 @Measurement(iterations = 10)
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
+
 public class NewChunkBench {
 
-  @Param({"10000", "1000000"})
+  @Param({"1000", "10000"})
+  private int cols;
+  @Param({"1000", "100000"})
   private int rows;
-  private int rowInterval = 1000;
-  private double tolerance = 1e-10;
-  private String[] filenames = new String[]{"smalldata/jira/floatVals.csv", "smalldata/jira/integerVals.csv",
-          "smalldata/jira/longVals.csv", "smalldata/jira/doubleVals.csv", "smalldata/jira/bigDoubleVals.csv"};
-  private int fileRows = 100000;
+  private Chunk[] chunks;
+  private double[][] raw;
+  private static double baseD = Math.PI;
+  private static double baseF = 1.1;
+  private static double baseL = 10.0*Integer.MAX_VALUE;
 
   @Benchmark
-  public void testParseFloats() {
-    testsForDoubles(false, false, true);
-    testsForDoubles(true, false, true);
-    testParseFromFiles(0);
-  }
-
-  @Benchmark
-  public void testParseDoubles() {
-    testsForDoubles(false, false, false);
-    testsForDoubles(true, false, false);  // constants
-    testParseFromFiles(3);
-  }
-
-  @Benchmark
-  public void testParseBigDoubles() {
-    testsForDoubles(false, true, false);
-    testsForDoubles(true, true, false);
-    testParseFromFiles(4);
-  }
-
-  @Benchmark
-  public void testParseInteger() {
-    testsForIntegers(false);
-    testsForIntegers(true);
-    testParseFromFiles(1);
-  }
-
-  // todo: enable benchmark test after Spencer PR is in.
- // @Benchmark
-  public void testParseLong() {
-    testsForLongs(false);
-    testsForLongs(true);
-    testParseFromFiles(2);
-  }
-
-
-  public void testParseFromFiles(int index) {
-    Frame f = parse_test_file(filenames[index]);  // parse from file
-    assertTrue(f.numRows() == fileRows);
-    if (f != null)
-      f.delete();
-  }
-
-  public void testsForLongs(boolean forConstants) {
-    final long baseD = Long.MAX_VALUE - 10 * (long) rows;
-
-    Vec tVec = Vec.makeZero(rows);
-    Vec v;
-    if (forConstants)
-      v = new MRTask() {
-        @Override
-        public void map(Chunk cs) {
-          for (int r = 0; r < cs._len; r++) {
-            cs.set(r, baseD);
-          }
-        }
-      }.doAll(tVec)._fr.vecs()[0];
-    else
-      v = new MRTask() {
-        @Override
-        public void map(Chunk cs) {
-          long rowStart = cs.start();
-          for (int r = 0; r < cs._len; r++) {
-            cs.set(r, r + baseD + rowStart);
-          }
-        }
-      }.doAll(tVec)._fr.vecs()[0];
-
-    for (int rowInd = 0; rowInd < rows; rowInd = rowInd + rowInterval) {
-      if (forConstants)
-        assertTrue("rowIndex: " + rowInd + " rowInd+baseD: " + (rowInd + baseD) + " v.at(rowIndex): "
-                        + v.at8(rowInd) + " chk= " + v.elem2ChunkIdx(rowInd),
-                v.at8(rowInd) == baseD);
-      else
-        assertTrue("rowIndex: " + rowInd + " rowInd+baseD: " + (rowInd + baseD) + " v.at(rowIndex): "
-                        + v.at8(rowInd) + " chk= " + v.elem2ChunkIdx(rowInd),
-                v.at8(rowInd) == (rowInd + baseD));
+  public double rawArrayRead() {
+    double sum = 0;
+    for (int col = 0; col < cols; ++col) {
+      for (int row = 0; row < rows; ++row) {
+        sum += raw[col][row];
+      }
     }
-    if (tVec != null)
-      tVec.remove();
-    if (v != null)
-      v.remove();
+    return sum;
   }
 
-  public void testsForIntegers(boolean forConstants) {
-    final int baseD = Integer.MAX_VALUE - 2 * rows;
-
-    Vec tVec = Vec.makeZero(rows);
-    Vec v;
-    if (forConstants)
-      v = new MRTask() {
-        @Override
-        public void map(Chunk cs) {
-          for (int r = 0; r < cs._len; r++) {
-            cs.set(r, baseD);
-          }
-        }
-      }.doAll(tVec)._fr.vecs()[0];
-    else
-      v = new MRTask() {
-        @Override
-        public void map(Chunk cs) {
-          long rowStart = cs.start();
-          for (int r = 0; r < cs._len; r++) {
-            cs.set(r, r + baseD + rowStart);
-          }
-        }
-      }.doAll(tVec)._fr.vecs()[0];
-
-    for (int rowInd = 0; rowInd < rows; rowInd = rowInd + rowInterval) {
-      if (forConstants)
-        assertTrue("rowIndex: " + rowInd + " rowInd+baseD: " + (rowInd + baseD) + " v.at(rowIndex): "
-                        + v.at8(rowInd) + " chk= " + v.elem2ChunkIdx(rowInd),
-                v.at8(rowInd) == baseD);
-      else
-        assertTrue("rowIndex: " + rowInd + " rowInd+baseD: " + (rowInd + baseD) + " v.at8(rowIndex): "
-                        + v.at8(rowInd) + " chk= " + v.elem2ChunkIdx(rowInd),
-                v.at8(rowInd) == (rowInd + baseD));
+  @Benchmark
+  public double rowsColsRead() {
+    double sum = 0;
+    for (int row = 0; row < rows; ++row) {
+      for (int col = 0; col < cols; ++col) {
+        sum += chunks[col].atd(row);
+      }
     }
-
-    if (tVec != null)
-      tVec.remove();
-    if (v != null)
-      v.remove();
+    return sum;
   }
 
-  public void testsForDoubles(boolean forConstants, boolean bigDouble, boolean forFloat) {
-
-    final double baseD = bigDouble ? (double) Long.MAX_VALUE + 1 : (forFloat ? 1.1 : Math.PI);
-
-
-    Vec tVec = Vec.makeZero(rows);
-    Vec v;
-    if (forConstants)
-      v = new MRTask() {
-        @Override
-        public void map(Chunk cs) {
-          for (int r = 0; r < cs._len; r++) {
-            cs.set(r, baseD);
-          }
-        }
-      }.doAll(tVec)._fr.vecs()[0];
-    else
-      v = new MRTask() {
-        @Override
-        public void map(Chunk cs) {
-          long rowStart = cs.start();
-          for (int r = 0; r < cs._len; r++) {
-            cs.set(r, baseD + rowStart + r);
-          }
-        }
-      }.doAll(tVec)._fr.vecs()[0];
-
-    for (int rowInd = 0; rowInd < rows; rowInd = rowInd + rowInterval) {
-      if (forConstants)
-        assertTrue("rowIndex: " + rowInd + " rowInd+baseD: " + (baseD) + " v.at(rowIndex): "
-                        + v.at(rowInd) + " chk= " + v.elem2ChunkIdx(rowInd),
-                Math.abs(v.at(rowInd) - baseD) / Math.max(v.at(rowInd), baseD) < tolerance);
-      else
-        assertTrue("rowIndex: " + rowInd + " rowInd+baseD: " + (baseD) + " v.at(rowIndex): "
-                        + v.at(rowInd) + " chk= " + v.elem2ChunkIdx(rowInd),
-                (Math.abs(v.at(rowInd) - (baseD + rowInd))) / Math.max(v.at(rowInd), (baseD + rowInd)) < tolerance);
+  @Benchmark
+  public double colsRowsRead() {
+    double sum = 0;
+    for (int col = 0; col < cols; ++col) {
+      for (int row = 0; row < rows; ++row) {
+        sum += chunks[col].atd(row);
+      }
     }
-    if (tVec != null)
-      tVec.remove();
-    if (v != null)
-      v.remove();
+    return sum;
+  }
 
+  @Benchmark
+  public double colsRowsReadWithTypeDispatch() {
+    double sum = 0;
+    for (int col = 0; col < cols; ++col) {
+      sum += walkChunk(rows, chunks[col]);
+    }
+    return sum;
+  }
+
+  @Benchmark
+  public double colsRowsWithBulkRead() {
+    double sum = 0;
+    // Preallocate array for storing unpacked chunk data
+    double [] vals = new double[chunks[0]._len];
+    for (int col = 0; col < cols; ++col) {
+      sum += walkChunkBulk(rows, chunks[col], vals);
+    }
+    return sum;
+  }
+
+  @Benchmark
+  public double colsRowsReadWithFinalChunk() {
+    double sum = 0;
+    for (int col = 0; col < cols; ++col) {
+      final Chunk c = chunks[col];
+      for (int row = 0; row < rows; ++row) {
+        sum += c.atd(row);
+      }
+    }
+    return sum;
+  }
+
+  private static double walkChunk(int rows, final Chunk c) {
+    double sum =0;
+    for (int row = 0; row < rows; ++row) {
+      sum += c.atd(row);
+    }
+    return sum;
+  }
+
+  private static double walkChunkBulk(int rows, final Chunk c, double [] vals) {
+    double sum = 0;
+    c.getDoubles(vals, 0, c._len);
+    for (int i = 0; i < rows; ++i)
+      sum += vals[i];
+    return sum;
+  }
+
+  @Setup
+  public void setup() {
+    raw = new double[cols][rows];
+    for (int col = 0; col < cols; ++col) {
+      for (int row = 0; row < rows; ++row) {
+        raw[col][row] = get(col, row);
+      }
+    }
+    chunks = new Chunk[cols];
+    for (int col = 0; col < cols; ++col) {
+      chunks[col] = new NewChunk(raw[col]).compress();
+    }
+  }
+
+  private static double get(int j, int i) {
+    switch (j % 4) { // do 4 chunk types
+      case 0:
+        return i % 200; //C1NChunk - 1 byte integer
+      case 1:
+        return i % 500; //C2Chunk - 2 byte integer
+      case 2:
+        return  i*Integer.MAX_VALUE;
+      case 3:
+        return i == 17 ? 1 : 0; //CX0Chunk - sparse
+      case 4: // long
+        return baseL+j+i; // long
+      case 5:
+        return baseF+i;  // float point
+      case 6:
+        return baseD+i+j; // double
+      default:
+        throw H2O.unimpl();
+    }
   }
 
   public static void main(String[] args) throws RunnerException {
     Options opt = new OptionsBuilder()
-            .include(NewChunkBench.class.getSimpleName())
+            .include(ChunkBench.class.getSimpleName())
             .addProfiler(StackProfiler.class)
             .build();
 
